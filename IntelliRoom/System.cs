cb7bb8d9c86cad1cae465fd546ffc8ba322edfe9
Rafa.camas@gmail.com <Rafa.camas@gmail.com@80cb36cf-2d85-3956-2fbf-893ae3530ec0>
2011-03-31 11:52:24 +0000

    Resuelto problemas con la union de proyectos y eliminacion de proyectos de TEST que no tenian ninguna utilidad

--- IntelliRoom/System.cs
+++ IntelliRoom/System.cs
@@ -13,7 +13,7 @@ namespace IntelliRoom
     {
         public static Language language = new Spanish();
         public static IVoiceEngine voiceEngine = new VoiceEngine();
-        public static ILighting lighting = new Lighting.Lighting();
+        public static ILighting lighting = new Lighting();
         public static IMediaPlayer media = new Media.MediaPlayer();
         public static InterpreterSpeech speechInterpreter = new InterpreterSpeech();
     }

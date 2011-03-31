cabf5213a61226f70c5f67b3d71564e93a919ac5
Rafa.camas@gmail.com <Rafa.camas@gmail.com@80cb36cf-2d85-3956-2fbf-893ae3530ec0>
2011-03-31 12:07:39 +0000

    Pequeños cambios en Electric.cs

--- trunk/Arduino/Electric.cs
+++ trunk/Arduino/Electric.cs
@@ -7,5 +7,25 @@ namespace Arduino
 {
     class Electric
     {
+        private Serial serialPort;
+        private Dictionary<int, string> disposivos;
+        
+        public Electric()
+        {
+            serialPort = new Serial();
+        }
+
+        public void SwitchOn (int numDisp)
+        {
+            serialPort.Write("SWITCHON "+numDisp.ToString());
+        }
+
+
+
+        public void SwitchOff (int numDisp)
+        {
+            serialPort.Write("SWITCHOFF "+numDisp.ToString());
+        }
+
     }
 }

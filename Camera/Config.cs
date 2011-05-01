9539ca12ce173ad6f0e63c853536e0be5ec13c3a
Rafa.camas@gmail.com <Rafa.camas@gmail.com@80cb36cf-2d85-3956-2fbf-893ae3530ec0>
2011-05-01 16:16:16 +0000

    Comenzamos a trabajar con el tratamiento de imagenes, creamos las clases y las primeras estructuras a raiz de trabajo que teniamos realizado de antes

--- Camera/Config.cs
+++ Camera/Config.cs
@@ -5,16 +5,28 @@ using System.Text;
 
 namespace Image
 {
-    static class Config
+    class Config
     {
         public static Camera camera = new Camera();
+
         public static int captureFPS = 4;
+
         public static int isMovement = 10;
+        public static int iluminanceEvent = 10;
+
         public static bool calculeIluminance = true;
         public static bool calculeMovement = true;
         public static bool calculeFace = true;
+
         public static bool saveMovement = true;
         public static bool saveFaces = true;
+    }
 
+    class LastResults
+    {
+        public static double movement;
+        public static FaceResult faces;
+        public static double iluminance;
+        public static int numberOfFaces;
     }
 }
---
+++ Camera/ImageEngine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Image
+{
+    class ImageEngine
+    {
+        public event EventHandler movementDetected;
+        public event EventHandler facesDetected;
+        public event EventHandler iluminanceEvent;
+
+        public Image<Bgr, Byte> image;
+        public Image<Bgr, Byte> lastImage;
+
+        public ImageEngine() { }
+
+        public void ProcessImage()
+        {
+            GetImage();
+
+            if (Config.calculeIluminance)
+            {
+                double iluminance = ImageUtils.GetIluminance(image);
+                LastResults.iluminance = iluminance;
+                if (iluminance >= Config.iluminanceEvent)
+                {
+                    
+                }
+            }
+
+            if (Config.calculeMovement)
+            {
+                double movement = ImageUtils.GetMovement(image, lastImage);
+                LastResults.movement = movement;
+                if (movement >= Config.isMovement)
+                {
+
+                }
+            }
+
+            if (Config.calculeFace)
+            {
+                FaceResult faceResult = ImageUtils.FaceDetect(image);
+                LastResults.faces = faceResult;
+                LastResults.numberOfFaces = faceResult.GetNumberOfFaces();
+                if (faceResult.FaceDetect())
+                {
+
+                }
+            }
+
+        }
+        private void GetImage()
+        {
+            if (lastImage == null)
+            {
+                lastImage = Config.camera.GetImage();
+            }
+
+            image = Config.camera.GetImage();
+        }
+    }
+}

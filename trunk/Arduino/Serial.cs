fecccb1e04bb49ea5486dede6dfceedb04882246
Rafa.camas@gmail.com <Rafa.camas@gmail.com@80cb36cf-2d85-3956-2fbf-893ae3530ec0>
2011-05-06 19:51:02 +0000

    Añadidas muchas modificaciones en el modulo Arduino -Sistema para reconocer arduino -Correccion en metodos -Solucionador algunos problemas de conexion con puertos
    -Añadida vercion de arduino que soporta reconocimiento de dispositivo
    -Añadida clase mensajeria para informacion y errores
    -Eliminada clase Config
    -Eliminados metodos de la consola

--- trunk/Arduino/Serial.cs
+++ trunk/Arduino/Serial.cs
@@ -33,10 +33,65 @@ namespace Arduino
 
         public Serial ()
         {
-            serial = new SerialPort("COM" + Data.Config.portComArduino.ToString(), 9600) { NewLine = "\r" };
-            serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(serial_DataReceived);
-            if(SerialPort.GetPortNames().Contains("COM" + Data.Config.portComArduino.ToString()) && !serial.IsOpen) 
-                serial.Open();
+            string PortCom = searchArduino();
+            if (PortCom != "NULL")
+            {
+                serial = new SerialPort(PortCom, 9600) { NewLine = "\r\n" };
+                serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(serial_DataReceived);
+
+                if (!serial.IsOpen)
+                    serial.Open();
+            }
+            else
+            {
+                Message.ErrorMessage("Arduino no encontrado");
+            }
+        }
+
+        /// <summary>
+        /// Devuelve el nombre del puerto donde esta conectado arduino
+        /// </summary>
+        /// <returns>En caso de no existir devuelve "NULL"</returns>
+        private string searchArduino()
+        {
+            string[] serialPortsName = SerialPort.GetPortNames();
+            string portCom = "NULL";
+
+            foreach (var elem in serialPortsName)
+            {
+                SerialPort serialPort = new SerialPort(elem, 9600) { NewLine = "\r\n" };
+                serialPort.ReadTimeout = 500;
+                bool res = false;
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.Open();
+                    res = IsArduino(serialPort);
+                    serialPort.Close();
+                }
+                else
+                {
+                    res = IsArduino(serialPort);
+                }
+                if (res)
+                {
+                    portCom = elem;
+                    break;
+                }
+            }
+            return portCom;
+        }
+
+        private bool IsArduino(SerialPort serialPort)
+        {
+            serialPort.WriteLine("CHECK");
+            try
+            {
+                return serialPort.ReadLine() == "ACK";
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         void  serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
---
+++ trunk/Data/Message.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data
+{
+    public class Message
+    {
+        static List<string> InfoMessages = new List<string>();
+        static List<string> ErrorMessages = new List<string>();
+
+        public static void InformationMessage(string information)
+        {
+            InfoMessages.Add(information);
+            System.Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.WriteLine("Info: " + information);
+            System.Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void ErrorMessage(string error)
+        {
+            ErrorMessages.Add(error);
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine("Error: " + error);
+            System.Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void ShowInformationMessage(int numMessages)
+        {
+            for (int i = numMessages; i < InfoMessages.Count; i++)
+            {
+                InformationMessage(InfoMessages[i]);
+            }
+        }
+
+        public static void ShowErrorMessage(int numMessages)
+        {
+            for (int i = numMessages; i < InfoMessages.Count; i++)
+            {
+                ErrorMessage(ErrorMessages[i]);
+            }
+        }
+
+    }
+}

48d038093646a5dd8954dfb14a32e9cc7beb21ac
Rafa.camas@gmail.com <Rafa.camas@gmail.com@80cb36cf-2d85-3956-2fbf-893ae3530ec0>
2011-05-08 09:58:06 +0000

    Reescrita y montada la version de console, ahora funciona todo de nuevo.

---
+++ trunk/Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Start());
+            CommandInterpreter commInter = new CommandInterpreter();
+
+            while (true)
+            {
+                System.Console.Write(">: ");
+                String command = System.Console.ReadLine();
+                System.Console.WriteLine(commInter.Interpreter(command));
+                System.Console.WriteLine("-------------------------------------------------");
+            }
+        }    
+
+        public static String Start()
+        {
+            //Llamo al Init de comando, solo deberia llamarse una vez
+            IntelliRoom.Command.Init();
+            //devuelvo el texto de entrada
+            return "Wellcome to IntelliRoom\n";
+        }
+    }
+}

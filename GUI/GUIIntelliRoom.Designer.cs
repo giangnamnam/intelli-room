b99e70631962177bbe6bcb96013c9fa7e9427f1e
Rafa.camas@gmail.com <Rafa.camas@gmail.com@80cb36cf-2d85-3956-2fbf-893ae3530ec0>
2011-05-19 10:27:24 +0000

    Añadida funcionalidad en la GUI: -Ejecucion de metodos y ejecucion de metodos en cadena -Busqueda de funciones a tiempo real -Actualizacion del historyList

--- GUI/GUIIntelliRoom.Designer.cs
+++ GUI/GUIIntelliRoom.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GUI
 {
-    partial class IntelliRoom
+    partial class GUIIntelliRoom
     {
         /// <summary>
         /// Required designer variable.
@@ -30,26 +30,26 @@
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.helpList = new System.Windows.Forms.ListBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.errorList = new System.Windows.Forms.ListBox();
+            this.historyList = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.commandBox = new System.Windows.Forms.TextBox();
             this.execute = new System.Windows.Forms.Button();
-            this.historyList = new System.Windows.Forms.ListBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.helpList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.errorList = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -88,31 +88,29 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(382, 410);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // groupBox1
+            // groupBox4
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.errorList);
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(321, 413);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Errores e información";
+            this.groupBox4.Controls.Add(this.helpList);
+            this.groupBox4.Location = new System.Drawing.Point(3, 69);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(376, 91);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Ayuda";
             // 
-            // groupBox2
+            // helpList
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            this.helpList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.tableLayoutPanel2);
-            this.groupBox2.Location = new System.Drawing.Point(3, 3);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(376, 60);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Comandos";
+            this.helpList.FormattingEnabled = true;
+            this.helpList.Location = new System.Drawing.Point(6, 17);
+            this.helpList.Name = "helpList";
+            this.helpList.Size = new System.Drawing.Size(364, 69);
+            this.helpList.TabIndex = 1;
             // 
             // groupBox3
             // 
@@ -127,41 +125,6 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Historial";
             // 
-            // errorList
-            // 
-            this.errorList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.errorList.FormattingEnabled = true;
-            this.errorList.Location = new System.Drawing.Point(6, 19);
-            this.errorList.Name = "errorList";
-            this.errorList.Size = new System.Drawing.Size(306, 381);
-            this.errorList.TabIndex = 0;
-            // 
-            // commandBox
-            // 
-            this.commandBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.commandBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.commandBox.Location = new System.Drawing.Point(3, 3);
-            this.commandBox.Multiline = true;
-            this.commandBox.Name = "commandBox";
-            this.commandBox.Size = new System.Drawing.Size(250, 29);
-            this.commandBox.TabIndex = 0;
-            // 
-            // execute
-            // 
-            this.execute.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.execute.Location = new System.Drawing.Point(259, 3);
-            this.execute.Name = "execute";
-            this.execute.Size = new System.Drawing.Size(102, 29);
-            this.execute.TabIndex = 1;
-            this.execute.Text = "Ejecutar";
-            this.execute.UseVisualStyleBackColor = true;
-            // 
             // historyList
             // 
             this.historyList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
@@ -173,18 +136,18 @@
             this.historyList.Size = new System.Drawing.Size(364, 212);
             this.historyList.TabIndex = 0;
             // 
-            // groupBox4
+            // groupBox2
             // 
-            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox4.Controls.Add(this.helpList);
-            this.groupBox4.Location = new System.Drawing.Point(3, 69);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(376, 91);
-            this.groupBox4.TabIndex = 2;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Ayuda";
+            this.groupBox2.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(376, 60);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Comandos";
             // 
             // tableLayoutPanel2
             // 
@@ -203,36 +166,75 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(364, 35);
             this.tableLayoutPanel2.TabIndex = 3;
             // 
-            // helpList
+            // commandBox
             // 
-            this.helpList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            this.commandBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.helpList.FormattingEnabled = true;
-            this.helpList.Location = new System.Drawing.Point(6, 17);
-            this.helpList.Name = "helpList";
-            this.helpList.Size = new System.Drawing.Size(364, 69);
-            this.helpList.TabIndex = 1;
+            this.commandBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commandBox.Location = new System.Drawing.Point(3, 3);
+            this.commandBox.Multiline = true;
+            this.commandBox.Name = "commandBox";
+            this.commandBox.Size = new System.Drawing.Size(250, 29);
+            this.commandBox.TabIndex = 0;
+            this.commandBox.TextChanged += new System.EventHandler(this.UpdateHelp);
             // 
-            // IntelliRoom
+            // execute
+            // 
+            this.execute.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.execute.Location = new System.Drawing.Point(259, 3);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(102, 29);
+            this.execute.TabIndex = 1;
+            this.execute.Text = "Ejecutar";
+            this.execute.UseVisualStyleBackColor = true;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.errorList);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 413);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Errores e información";
+            // 
+            // errorList
+            // 
+            this.errorList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorList.FormattingEnabled = true;
+            this.errorList.Location = new System.Drawing.Point(6, 19);
+            this.errorList.Name = "errorList";
+            this.errorList.Size = new System.Drawing.Size(306, 381);
+            this.errorList.TabIndex = 0;
+            // 
+            // GUIIntelliRoom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 416);
             this.Controls.Add(this.splitContainer1);
-            this.Name = "IntelliRoom";
+            this.Name = "GUIIntelliRoom";
             this.Text = "IntelliRoom";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
---
+++ GUI/GUIIntelliRoom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class GUIIntelliRoom : Form
+    {
+        CommandInterpreter commInter;
+
+        public GUIIntelliRoom()
+        {
+            InitializeComponent();
+            //Ejecuto el comando inicial
+            commInter = new CommandInterpreter();
+            IntelliRoom.Command.Init();
+        }
+
+        private void execute_Click(object sender, EventArgs e)
+        {
+            String command = commandBox.Text;
+            commandBox.Text = "";
+            //añadimos el comando insertado
+            historyList.Items.Add("Execute: "+command);
+            //ejecutamos el comando
+            String resCommand = commInter.Interpreter(command);
+            //pintamos el resultad
+            historyList.Items.Add("Response: " + resCommand);
+        }
+
+        private void UpdateHelp(object sender, EventArgs e)
+        {
+            String command = commandBox.Text;
+            List<string> result = commInter.SearchHelp(command);
+
+            helpList.Items.Clear();
+            helpList.Items.AddRange(result.ToArray());
+        }
+    }
+}
